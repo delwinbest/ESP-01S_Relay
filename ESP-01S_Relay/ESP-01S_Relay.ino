98017db5c6f68e13e2e85535b656c93ca38e597b
Delwin Best <mail@delwinbest.com>
2019-09-04 22:18:27 +0900

    added general password to AP and status

--- ESP-01S_Relay/ESP-01S_Relay.ino
+++ ESP-01S_Relay/ESP-01S_Relay.ino
@@ -9,6 +9,7 @@ String header;
 
 // Auxiliar variables to store the current output state
 String relayState = "off";
+bool configMode = false;
 
 // Current time
 unsigned long currentTime = millis();
@@ -20,10 +21,12 @@ const long timeoutTime = 2000;
 //gets called when WiFiManager enters configuration mode
 void configModeCallback (WiFiManager *myWiFiManager) {
   //entered config mode
+  configMode = true;
 }
 
 void setup() {
   delay(1000);
+  configMode = false;
   // Initialize the output variables as outputs
   pinMode(relayPin, OUTPUT);
 
@@ -42,7 +45,7 @@ void setup() {
   //if it does not connect it starts an access point with the specified name
   //here  "AutoConnectAP"
   //and goes into a blocking loop awaiting configuration
-  if (!wm.autoConnect()) {
+  if (!wm.autoConnect(WIFI_HOSTNAME,"pa55w0rd")) {
     //reset and try again, or maybe put it to deep sleep
     ESP.restart();
     delay(1000);
@@ -59,105 +62,108 @@ void setup() {
   ArduinoOTA.onError([](ota_error_t error) {
   });
   // Start the server
-  server.begin();
+  if ( configMode == false ) {
+    server.begin();
+  }
   ArduinoOTA.begin();
 }
 
 
 void loop() {
   ArduinoOTA.handle();
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        String responseType = "html";
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /html/relay/on") >= 0) {
-              relayState = "on";
-              digitalWrite(relayPin, LOW);
-              responseType = "html";
-            } else if (header.indexOf("GET /html/relay/off") >= 0) {
-              relayState = "off";
-              digitalWrite(relayPin, HIGH);
-              responseType = "html";
-            } else if (header.indexOf("GET /relay/off") >= 0) {
-              relayState = "off";
-              digitalWrite(relayPin, HIGH);
-              responseType = "cli";
-            } else if (header.indexOf("GET /relay/on") >= 0) {
-              relayState = "on";
-              digitalWrite(relayPin, HIGH);
-              responseType = "cli";
-            } else if (header.indexOf("GET /status") >= 0) {
-              responseType = "cli";
-            } 
-
-            if (responseType == "html") {
-              // Display the HTML web page
-              client.println("<!DOCTYPE html><html>");
-              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-              client.println("<link rel=\"icon\" href=\"data:,\">");
-              // CSS to style the on/off buttons 
-              // Feel free to change the background-color and font-size attributes to fit your preferences
-              client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-              client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-              client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-              client.println(".button2 {background-color: #77878A;}</style></head>");
+  if ( configMode == false ) {
+    WiFiClient client = server.available();   // Listen for incoming clients
+  
+    if (client) {                             // If a new client connects,
+      String currentLine = "";                // make a String to hold incoming data from the client
+      currentTime = millis();
+      previousTime = currentTime;
+      while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+        currentTime = millis();         
+        if (client.available()) {             // if there's bytes to read from the client,
+          char c = client.read();             // read a byte, then
+          String responseType = "html";
+          header += c;
+          if (c == '\n') {                    // if the byte is a newline character
+            // if the current line is blank, you got two newline characters in a row.
+            // that's the end of the client HTTP request, so send a response:
+            if (currentLine.length() == 0) {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
               
-              // Web Page Heading
-              client.println("<body><h1>ESP8266 Web Server</h1>");
-              
-              // Display current state, and ON/OFF buttons for GPIO 5  
-              client.println("<p>Relay - State " + relayState + "</p>");
-              // If the relayState is off, it displays the ON button       
-              if (relayState=="off") {
-                client.println("<p><a href=\"/html/relay/on\"><button class=\"button\">ON</button></a></p>");
-              } else {
-                client.println("<p><a href=\"/html/relay/off\"><button class=\"button button2\">OFF</button></a></p>");
+              // turns the GPIOs on and off
+              if (header.indexOf("GET /html/relay/on") >= 0) {
+                relayState = "on";
+                digitalWrite(relayPin, LOW);
+                responseType = "html";
+              } else if (header.indexOf("GET /html/relay/off") >= 0) {
+                relayState = "off";
+                digitalWrite(relayPin, HIGH);
+                responseType = "html";
+              } else if (header.indexOf("GET /relay/off") >= 0) {
+                relayState = "off";
+                digitalWrite(relayPin, HIGH);
+                responseType = "cli";
+              } else if (header.indexOf("GET /relay/on") >= 0) {
+                relayState = "on";
+                digitalWrite(relayPin, HIGH);
+                responseType = "cli";
+              } else if (header.indexOf("GET /status") >= 0) {
+                responseType = "cli";
               } 
-              client.println("</body></html>");
-            } else if (responseType == "cli") {
-              if (relayState=="off") {
-                client.println("0");
-              } else {
-                client.println("1");
-              }               
+  
+              if (responseType == "html") {
+                // Display the HTML web page
+                client.println("<!DOCTYPE html><html>");
+                client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                client.println("<link rel=\"icon\" href=\"data:,\">");
+                // CSS to style the on/off buttons 
+                // Feel free to change the background-color and font-size attributes to fit your preferences
+                client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+                client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                client.println(".button2 {background-color: #77878A;}</style></head>");
+                
+                // Web Page Heading
+                client.println("<body><h1>ESP8266 Web Server</h1>");
+                
+                // Display current state, and ON/OFF buttons for GPIO 5  
+                client.println("<p>Relay - State " + relayState + "</p>");
+                // If the relayState is off, it displays the ON button       
+                if (relayState=="off") {
+                  client.println("<p><a href=\"/html/relay/on\"><button class=\"button\">ON</button></a></p>");
+                } else {
+                  client.println("<p><a href=\"/html/relay/off\"><button class=\"button button2\">OFF</button></a></p>");
+                } 
+                client.println("</body></html>");
+                // The HTTP response ends with another blank line
+                client.println();
+              } else if (responseType == "cli") {
+                if (relayState=="off") {
+                  client.println("0");
+                } else {
+                  client.println("1");
+                }               
+              }
+              // Break out of the while loop
+              break;
+            } else { // if you got a newline, then clear currentLine
+              currentLine = "";
             }
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
+          } else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
         }
       }
+      // Clear the header variable
+      header = "";
+      // Close the connection
+      client.stop();
+  
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-
   }
-
 }
