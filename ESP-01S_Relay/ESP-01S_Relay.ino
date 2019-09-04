debdc8a8d26483737b180f109bb1908840f91c7a
Delwin Best <mail@delwinbest.com>
2019-09-04 20:47:07 +0900

    http client rework

--- ESP-01S_Relay/ESP-01S_Relay.ino
+++ ESP-01S_Relay/ESP-01S_Relay.ino
@@ -4,6 +4,22 @@
 
 WiFiServer server(80);
 
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String relayState = "off";
+
+// Assign output variables to GPIO pins
+const int relayPin = 2;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 //gets called when WiFiManager enters configuration mode
 void configModeCallback (WiFiManager *myWiFiManager) {
   //entered config mode
@@ -11,8 +27,12 @@ void configModeCallback (WiFiManager *myWiFiManager) {
 
 void setup() {
   delay(1000);
-  pinMode(RELAY, OUTPUT); 
-  digitalWrite(RELAY,LOW);
+  // Initialize the output variables as outputs
+  pinMode(relayPin, OUTPUT);
+
+  // Set outputs to LOW
+  digitalWrite(relayPin, LOW);
+  
   //Serial.begin(115200);
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
@@ -32,7 +52,8 @@ void setup() {
     delay(1000);
   }
   WiFi.hostname(WIFI_HOSTNAME);
-  
+
+  ArduinoOTA.setHostname(WIFI_HOSTNAME);
   ArduinoOTA.onStart([]() {
     //Serial.println("Start updating ");
   });
@@ -55,66 +76,87 @@ void setup() {
   ArduinoOTA.begin();
 }
 
+
 void loop() {
   ArduinoOTA.handle();
-  WiFiClient client = server.available();
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (!client) 
-  {
-    return;
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /relay/on") >= 0) {
+              Serial.println("Relay on");
+              relayState = "on";
+              digitalWrite(relayPin, HIGH);
+            } else if (header.indexOf("GET /relay/off") >= 0) {
+              Serial.println("Relay off");
+              relayState = "off";
+              digitalWrite(relayPin, LOW);
+            } 
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.println("<p>Relay - State " + relayState + "</p>");
+            // If the relayState is off, it displays the ON button       
+            if (relayState=="off") {
+              client.println("<p><a href=\"/relay/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/relay/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
- 
-  // Wait until the client sends some data
-  //Serial.println("new client");
-  //while(!client.available())
-  //{
-  //  delay(1);
-  //}
-
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  //Serial.println(request);
-  client.flush();
- 
-  // Match the request
-  int value = LOW;
-  if (request.indexOf("/RELAY=ON") != -1)  
-  {
-    //Serial.println("RELAY=ON");
-    digitalWrite(RELAY,LOW);
-    value = LOW;
-  }
-  if (request.indexOf("/RELAY=OFF") != -1)  
-  {
-    //Serial.println("RELAY=OFF");
-    digitalWrite(RELAY,HIGH);
-    value = HIGH;
-  }
-  
-  // Return the response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  this is a must
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.println("<head><title>ESP8266 RELAY Control</title></head>");
-  client.print("Relay is now: ");
- 
-  if(value == HIGH) 
-  {
-    client.print("OFF");
-  } 
-  else 
-  {
-    client.print("ON");
-  }
-  client.println("<br><br>");
-  client.println("Turn <a href=\"/RELAY=OFF\">OFF</a> RELAY<br>");
-  client.println("Turn <a href=\"/RELAY=ON\">ON</a> RELAY<br>");
-  client.println("</html>");
-  client.stop();
-  delay(2);
-  //Serial.println("Client disonnected");
-  //Serial.println("");
 
 }
